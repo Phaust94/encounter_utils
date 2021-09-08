1695de34a2d38143e884380369c88b2e0af2fc7b
Anton Bogdanov <anton.bogdanov@gameloft.com>
2021-09-08 22:28:56 +0300

    Added possibility t ofix random seed

--- encounter_utils/utils.py
+++ encounter_utils/utils.py
@@ -1,6 +1,6 @@
 import typing
 
-from numpy.random import choice
+from numpy import random
 
 __all__ = [
     "generate_code",
@@ -11,14 +11,16 @@ EN_ALPAHBET = list("wertyupasdfghjkzxcvbn")
 NUMBERS = list("12456789")
 
 
-def generate_code(mask: str, n: int = 1) -> typing.List[str]:
+def generate_code(mask: str, n: int = 1, seed: int = None) -> typing.List[str]:
+    if seed:
+        random.seed(seed)
     codes = []
     for sym in mask:
         alphabet = {
             sym in x: x
             for x in [RU_ALPHABET, EN_ALPAHBET, NUMBERS]
         }[True]
-        let = choice(alphabet, n, True)
+        let = random.choice(alphabet, n, True)
         codes.append(let)
     codes = ["".join(seq) for seq in zip(*codes)]
     return codes
